4cf1fc8dd76833589bafa297bcbdf337886baec8
Michael Ross <mross.j@gmail.com>
2020-04-10 22:23:56 -0400

    add projects app
    * added requests for projects
    * GET, POST, and DELETE
    * added template styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Projects } from './components/projects.js';
-import { Route } from 'react-router-dom';
-
+import { Route, Redirect } from 'react-router-dom';
 import './App.scss';
 
 export const App = () => {
-  return (
-    <div className="App">
-      <>
-      <Route path='/projects'>
+	return (
+		<div className='App'>
+			<>
+				<Route path='/projects'>
 					<Projects />
 				</Route>
-        </>
-    </div>
-  );
-}
+				<Route exact path='/'>
+					<Redirect to='/projects' />
+				</Route>
+			</>
+		</div>
+	);
+};
