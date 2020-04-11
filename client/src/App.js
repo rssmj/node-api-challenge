60492360c75a681e4188c6d5608876478254b70c
Michael Ross <mross.j@gmail.com>
2020-04-10 20:25:02 -0400

    add projects react app
    * installed dependencies
    * added projects component
    * add projects route in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Projects } from './components/projects.js';
+import { Route } from 'react-router-dom';
+
 import './App.scss';
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
-        <>
+      <>
+      <Route path='/projects'>
+					<Projects />
+				</Route>
         </>
     </div>
   );
 }
-
-export default App;
